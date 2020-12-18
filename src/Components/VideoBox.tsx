c558f083a4c995cdf3df9b1d113a1de4076f9783
Ashutosh <ashutosh94g@gmail.com>
2020-12-18 19:48:54 +0530

    Error fix

--- src/Components/VideoBox.tsx
+++ src/Components/VideoBox.tsx
@@ -5,9 +5,9 @@ import "./VideoBox.css";
 const VideoBox = () => {
 	return (
 		<div className="VideoBox">
-			<img src="https://image.shutterstock.com/z/stock-photo-young-woman-at-exhibition-in-modern-art-gallery-1664204695.jpg" />
+			<img alt='' src="https://image.shutterstock.com/z/stock-photo-young-woman-at-exhibition-in-modern-art-gallery-1664204695.jpg" />
 			<div className="VideoBox-detail">
-				<img src="https://image.shutterstock.com/z/stock-photo-young-woman-at-exhibition-in-modern-art-gallery-1664204695.jpg" />
+				<img alt='' src="https://image.shutterstock.com/z/stock-photo-young-woman-at-exhibition-in-modern-art-gallery-1664204695.jpg" />
 				<div>
 					<h3>First Video</h3>
 					Channel name
